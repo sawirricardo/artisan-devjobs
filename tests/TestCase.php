b1c3a4a919b672ac2b9d6312ee5987d13d4c387c
sawirricardo <sawir.ricardo@gmail.com>
2022-10-12 17:51:59 +0700

    remove unneeded code

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -22,10 +22,5 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_artisan-devjobs_table.php.stub';
-        $migration->up();
-        */
     }
 }
